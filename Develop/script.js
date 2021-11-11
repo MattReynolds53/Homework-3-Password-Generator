45a1610206cec742dc7fb8b64b6897c52435ae2b
Matt Reynolds <mattreynolds53@gmail.com>
2021-11-11 14:08:23 -0600

    Removed else statements because they were not allowing the final password to properly concatinate the arrays. Also added a finalPassword variable and had it include each generated string so that the generated strings would not take the place of one another (lto avoid being left with only 1 character at the end of the random generation

--- Develop/script.js
+++ Develop/script.js
@@ -24,46 +24,65 @@ function writePassword() {
 function generatePassword() {
 
 
-  let PasswordLength = prompt("What length would you like your password to be? Please pick a number 8-128.");
-console.log(PasswordLength);
-;
-let choiceForLowercaseLetters = confirm("Would you like to include lowercase letters in your password? Please select 'OK' for yes or 'Cancel' for no.")
+let passwordLength = prompt("What length would you like your password to be? Please pick a number 8-128.");
+console.log(passwordLength);
+
+let choiceForLowercaseLetters = confirm("Would you like to include lowercase letters in your password? Please select 'OK' for yes or 'Cancel' for no.");
 console.log(choiceForLowercaseLetters);
 
 if (choiceForLowercaseLetters === true) {
   concatinatedArrayPassword = [...lowercaseAlphabet, ...concatinatedArrayPassword]; //do the same thing you did here for all of them
-} else {
-  concatinatedArrayPassword = [...concatinatedArrayPassword];
-};
+} ;
+// else {
+//   concatinatedArrayPassword = [...concatinatedArrayPassword];
+// };
 
 let choiceForUppercaseLetters = confirm("Would you like to include uppercase letters in your password? Please select 'OK' for yes or 'Cancel' for no.");
 console.log(choiceForUppercaseLetters);
 
 if (choiceForUppercaseLetters === true) {
   concatinatedArrayPassword = [...uppercaseAlphabet, ...concatinatedArrayPassword]; //do the same thing you did here for all of them
-} else {
-  concatinatedArrayPassword = [...concatinatedArrayPassword];
-};
+} ;
+// else {
+//   concatinatedArrayPassword = [...concatinatedArrayPassword];
+// };
 
 let choiceForNumbers = confirm("Would you like to include numbers in your password? Please select 'OK' for yes or 'Cancel' for no.");
 console.log(choiceForNumbers);
 
 if (choiceForNumbers === true) {
   concatinatedArrayPassword = [...numbers, ...concatinatedArrayPassword]; //do the same thing you did here for all of them
-} else {
-  concatinatedArrayPassword = [...concatinatedArrayPassword];
-};
+} ;
+// else {
+//   concatinatedArrayPassword = [...concatinatedArrayPassword];
+// };
 
 let choiceForSpecialCharacters = confirm("Would you like to include special characters in your password? Please select 'OK' for yes or 'Cancel' for no.");
 console.log(choiceForSpecialCharacters);
 
 if (choiceForSpecialCharacters === true) {
   concatinatedArrayPassword = [...specialCharacters, ...concatinatedArrayPassword]; //do the same thing you did here for all of them
-} else {
-  concatinatedArrayPassword = [...concatinatedArrayPassword];
-}
+} ;
+// else {
+//   concatinatedArrayPassword = [...concatinatedArrayPassword];
+// }
 
-  return password; //you need to return your password at the end or else it will come back undefined. 
+// If I have my if statements separated, will that still concatinate the appropriate arrys based on the user selections for the confirms? Or should I compile my separated if statements into one if, else if, else statement?
+console.log(concatinatedArrayPassword)
+
+let finalPassword = "";
+
+
+// let password = "";
+for (let i = 0; i < passwordLength; i++) {
+  let numberRandom = Math.floor(Math.random()*concatinatedArrayPassword.length);
+  finalPassword = finalPassword + concatinatedArrayPassword[numberRandom];
+  console.log(concatinatedArrayPassword[numberRandom]);
+  //need to get all individual string into one combined strings (wihtin the For loop)
+}
+console.log(finalPassword)
+
+  return finalPassword; //you need to return your password at the end or else it will come back undefined. 
 }
 
 // Add event listener to generate button
@@ -73,9 +92,5 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// let password = "";
-// for (let i = 0; i < 128; i++) {
-  
-  
-// }
+
 
