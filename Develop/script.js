badece3c7b99a74e04f0139bbccbebcf318956be
Matt Reynolds <mattreynolds53@gmail.com>
2021-11-11 08:40:43 -0600

    Globalized arrays, added concatinatedArrayPassword, and began writing if statements for the user responses from the prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+let lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialCharacters = [")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "-", "_", "+", "=", "[", "]", "{", "}", "`", "~"];
+let concatinatedArrayPassword = []
+
+let randomLowercaseAlphabet = Math.floor(Math.random()*lowercaseAlphabet.length);
+let randomUppercaseAlphabet = Math.floor(Math.random()*uppercaseAlphabet.length);
+let randomNumbers = Math.floor(Math.random()*numbers.length);
+let randomSpecialCharacter =Math.floor(Math.random()*specialCharacters.length);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,33 +21,48 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+
+  return "generatePassword"; //you need to return your password at the end or else it will come back undefined. 
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-let firstPasswordChoice = prompt("What length would you like your password to be? Please pick a number 8-128.");
-console.log(firstPasswordChoice);
+let PasswordLength = prompt("What length would you like your password to be? Please pick a number 8-128.");
+console.log(PasswordLength);
 
-let secondPasswordChoice = prompt("Would you like to include lowercase letters in your password? Please type 'Y' for yes or 'N' for no.")
-console.log(secondPasswordChoice);
+let choiceForLowercaseLetters = prompt("Would you like to include lowercase letters in your password? Please type 'Y' for yes or 'N' for no.")
+console.log(choice);
 
-let thirdPasswordChoice = prompt("Would you like to include uppercase letters in your password? Please type 'Y' for yes or 'N' for no.")
+let choiceForUppercaseLetters = prompt("Would you like to include uppercase letters in your password? Please type 'Y' for yes or 'N' for no.")
 console.log(thirdPasswordChoice);
 
-let fourthPasswordChoice = prompt("Would you like to include numbers in your password? Please type 'Y' for yes or 'N' for no.")
+let choiceForNumbers = prompt("Would you like to include numbers in your password? Please type 'Y' for yes or 'N' for no.")
 console.log(fourthPasswordChoice);
 
-let fifthPasswordChoice = prompt("Would you like to include special characters in your password? Please type 'Y' for yes or 'N' for no.")
+let choiceForSpecialCharacters = prompt("Would you like to include special characters in your password? Please type 'Y' for yes or 'N' for no.")
 console.log(fifthPasswordChoice);
 
+if (choiceForLowercaseLetters === true) {
+  concatinatedArrayPassword = [...lowercaseAlphabet, ...concatinatedArrayPassword]; //do the same thing you did here for all of them
+} else {
+  concatinatedArrayPassword = [...concatinatedArrayPassword];
+}
 
-let lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let specialCharacters = [")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "-", "_", "+", "=", "[", "]", "{", "}", "`", "~"];
+if (choiceForLowercaseLetters === true) {
+  concatinatedArrayPassword = [...lowercaseAlphabet, ...concatinatedArrayPassword]; //do the same thing you did here for all of them
+} else {
+  concatinatedArrayPassword = [...concatinatedArrayPassword];
+}
+
+
+
 
 let password = "";
 for (let i = 0; i < 128; i++) {
-  const element = array[index];
+  
   
 }
 
